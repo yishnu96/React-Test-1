137de09f86419cf8e4445c90871019c94c252444
yishnu96 <yishnupramanik96@gmail.com>
2020-08-09 19:21:20 +0530

    tooltip added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import './App.css';
+import ReactTooltip from 'react-tooltip';
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: #0cbaba;
+  width: 150px;
+  margin: 30vh auto;
+  padding: 15px;
+  text-align: center;
+  font-size: large;
+  color: white;
+  border-radius: 50px;
+
+`
+const StyledReactTooltip = styled(ReactTooltip)`
+  background-color: #eeb868 !important;
+  padding: 15px !important;
+  color: white !important;
+  box-shadow: 0px 2px 20px black;
+  &:after {
+    border-top-color: #eeb868 !important;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Container data-tip data-for='download'> 
+          Download
+        </Container>
+        <StyledReactTooltip id='download' place='top' effect='solid'>
+          <span>File Size: 50MB</span>
+        </StyledReactTooltip>
+      </div>
   );
 }
 
